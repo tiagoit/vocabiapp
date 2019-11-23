0a371627bcf5926557f4b6c6ec113ef6ea8c2a97
Tiago Ferreira <tiferreira12@gmail.com>
2019-11-23 16:19:42 -0300

    adds jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("bootstrap")
+require("jquery")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
