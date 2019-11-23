897762f831d1d42605194e82e14cde126173735b
Tiago Ferreira <tiferreira12@gmail.com>
2019-11-23 19:14:12 -0300

    change app name to vocab app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = 'Flight Archer'
+    base_title = 'Vocabi app'
     if page_title.empty?
       base_title
     else
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "flight_archer_production"
+  # config.active_job.queue_name_prefix = "vocabiapp_production"
 
   config.action_mailer.perform_caching = false
 
