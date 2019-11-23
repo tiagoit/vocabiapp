af31a5950e37c946c4643f96c89368e2015c1bb0
Tiago Ferreira <tiferreira12@gmail.com>
2019-11-23 15:46:03 -0300

    add pos and levels list on home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    @pos = %w[nouns verbs adjectives prepositions adverbs]
+  end
 
+  def words_txt
+    render plain: Word.all.flat_map(&:en).join("\n")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get ':pos/:level', to: 'game#play'
+  get 'words-txt', to: 'static_pages#words_txt'
 end
