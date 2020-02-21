30dc4993e578904a272d46e51ba915c1b7026fae
Tiago Ferreira (tiagoit) <tiferreira12@gmail.com>
2020-02-21 20:50:08 -0300

    fix: clean firebase config

--- cra/src/firebase/firebase.js
+++ cra/src/firebase/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDSAl1pNQm2XOOrRrXSHcV0omANfnDTyFU',
-  authDomain: 'vocabiapp.firebaseapp.com',
-  databaseURL: 'https://vocabiapp.firebaseio.com',
-  projectId: 'vocabiapp',
-  storageBucket: 'vocabiapp.appspot.com',
-  messagingSenderId: '999016236067',
-  appId: '1:999016236067:web:53d29351cc490802209d96',
-  measurementId: 'G-BW0Q1LH8W2',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
